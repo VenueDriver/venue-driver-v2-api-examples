6059b989b776e64bef86780825a08b6f2e650dfc
Ryan Porter <rap@endymion.com>
2017-07-24 11:02:10 -0400

    Venues resource is public.
    Also improved feedback.

--- examples/list_venues.rb
+++ examples/list_venues.rb
@@ -7,18 +7,18 @@ require 'json'
 
 api_base_URL = ARGV[0] # http://api.example.comv1/
 
-puts "Authentication API resource URL: " +
-  (uri = URI.join(api_base_URL, 'auth')).to_s
-response = HTTParty.get(uri,
-  :query => {:username => ARGV[1], :password => ARGV[2]})
-puts "API response" +
-  response.body, response.code, response.message, response.headers.inspect
-puts "Authorization token: " + (token = JSON.parse(response.body)['token'])
+options =
+  if(ARGV[1])
+    {query: {account_id: ARGV[1]}}
+  else
+    {}
+  end
 
 puts "Venues API resource URL:" +
-  (uri = URI.join(api_base_URL, 'venues')).to_s
-response = HTTParty.get(uri,
-  :headers => {'Authorization' => token},
-  :query => {'account_id' => ARGV[3]})
-puts "API response: " +
-  response.body, response.code, response.message, response.headers.inspect
+  (uri = URI.join(api_base_URL, 'public/venues')).to_s
+response = HTTParty.get(uri, options)
+puts "API response: #{response.code} #{response.message}"
+# puts "API response headers: "
+# response.headers.each{|h| puts "#{h}: #{response.headers[h]}"}
+puts "API response body: "
+puts response.body
